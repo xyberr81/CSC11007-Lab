7b28c7ee43a0e7fbf948e921884432da451acb65
xyberr <sikea0801@gmail.com>
2026-06-13 15:53:59 +0700

    perf: comment out heavy monitoring and databases in setup-cluster.sh for low-memory run

--- k8s/deploy/setup-cluster.sh
+++ k8s/deploy/setup-cluster.sh
@@ -49,30 +49,30 @@ helm upgrade --install kafka-cluster ./kafka/kafka-cluster \
 --set postgresql.password="$POSTGRESQL_PASSWORD"
 
 #Install akhq
-akhq_hostname="akhq.$DOMAIN" yq -i '.hostname=env(akhq_hostname)' ./kafka/akhq.values.yaml
-helm upgrade --install akhq akhq/akhq \
---create-namespace --namespace kafka \
---values ./kafka/akhq.values.yaml
+# akhq_hostname="akhq.$DOMAIN" yq -i '.hostname=env(akhq_hostname)' ./kafka/akhq.values.yaml
+# helm upgrade --install akhq akhq/akhq \
+# --create-namespace --namespace kafka \
+# --values ./kafka/akhq.values.yaml
 
 #Install elastic-operator
-helm upgrade --install elastic-operator elastic/eck-operator \
- --create-namespace --namespace elasticsearch
+# helm upgrade --install elastic-operator elastic/eck-operator \
+#  --create-namespace --namespace elasticsearch
 
 # Install elasticsearch-cluster
-helm upgrade --install elasticsearch-cluster ./elasticsearch/elasticsearch-cluster \
---create-namespace --namespace elasticsearch \
---set elasticsearch.replicas="$ELASTICSEARCH_REPLICAES" \
---set kibana.ingress.hostname="kibana.$DOMAIN"
+# helm upgrade --install elasticsearch-cluster ./elasticsearch/elasticsearch-cluster \
+# --create-namespace --namespace elasticsearch \
+# --set elasticsearch.replicas="$ELASTICSEARCH_REPLICAES" \
+# --set kibana.ingress.hostname="kibana.$DOMAIN"
 
 #Install loki
-helm upgrade --install loki grafana/loki \
- --create-namespace --namespace observability \
- -f ./observability/loki.values.yaml
+# helm upgrade --install loki grafana/loki \
+#  --create-namespace --namespace observability \
+#  -f ./observability/loki.values.yaml
 
 #Install tempo
-helm upgrade --install tempo grafana/tempo \
---create-namespace --namespace observability \
--f ./observability/tempo.values.yaml
+# helm upgrade --install tempo grafana/tempo \
+# --create-namespace --namespace observability \
+# -f ./observability/tempo.values.yaml
 
 #Install cert manager
 helm upgrade --install cert-manager jetstack/cert-manager \
@@ -85,39 +85,39 @@ helm upgrade --install cert-manager jetstack/cert-manager \
   --set admissionWebhooks.certManager.create=true
 
 #Install opentelemetry-operator
-helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator \
---create-namespace --namespace observability
+# helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator \
+# --create-namespace --namespace observability
 
 #Install opentelemetry-collector
-helm upgrade --install opentelemetry-collector ./observability/opentelemetry \
---create-namespace --namespace observability
+# helm upgrade --install opentelemetry-collector ./observability/opentelemetry \
+# --create-namespace --namespace observability
 
 #Install promtail
-helm upgrade --install promtail grafana/promtail \
---create-namespace --namespace observability \
---values ./observability/promtail.values.yaml
+# helm upgrade --install promtail grafana/promtail \
+# --create-namespace --namespace observability \
+# --values ./observability/promtail.values.yaml
 
 #Install prometheus + grafana
-grafana_hostname="grafana.$DOMAIN" yq -i '.hostname=env(grafana_hostname)' ./observability/prometheus.values.yaml
-postgresql_username="$POSTGRESQL_USERNAME" yq -i '.grafana."grafana.ini".database.user=env(postgresql_username)' ./observability/prometheus.values.yaml
-postgresql_password="$POSTGRESQL_PASSWORD" yq -i '.grafana."grafana.ini".database.password=env(postgresql_password)' ./observability/prometheus.values.yaml
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
- --create-namespace --namespace observability \
--f ./observability/prometheus.values.yaml \
+# grafana_hostname="grafana.$DOMAIN" yq -i '.hostname=env(grafana_hostname)' ./observability/prometheus.values.yaml
+# postgresql_username="$POSTGRESQL_USERNAME" yq -i '.grafana."grafana.ini".database.user=env(postgresql_username)' ./observability/prometheus.values.yaml
+# postgresql_password="$POSTGRESQL_PASSWORD" yq -i '.grafana."grafana.ini".database.password=env(postgresql_password)' ./observability/prometheus.values.yaml
+# helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
+#  --create-namespace --namespace observability \
+# -f ./observability/prometheus.values.yaml \
 
 #Install grafana operator
-helm upgrade --install grafana-operator oci://ghcr.io/grafana-operator/helm-charts/grafana-operator \
---version v5.0.2 \
---create-namespace --namespace observability
+# helm upgrade --install grafana-operator oci://ghcr.io/grafana-operator/helm-charts/grafana-operator \
+# --version v5.0.2 \
+# --create-namespace --namespace observability
 
 #Add datasource and dashboard to grafana
-helm upgrade --install grafana ./observability/grafana \
---create-namespace --namespace observability \
---set hotname="grafana.$DOMAIN" \
---set grafana.username="$GRAFANA_USERNAME" \
---set grafana.password="$GRAFANA_PASSWORD" \
---set postgresql.username="$POSTGRESQL_USERNAME" \
---set postgresql.password="$POSTGRESQL_PASSWORD"
+# helm upgrade --install grafana ./observability/grafana \
+# --create-namespace --namespace observability \
+# --set hotname="grafana.$DOMAIN" \
+# --set grafana.username="$GRAFANA_USERNAME" \
+# --set grafana.password="$GRAFANA_PASSWORD" \
+# --set postgresql.username="$POSTGRESQL_USERNAME" \
+# --set postgresql.password="$POSTGRESQL_PASSWORD"
 
 helm upgrade --install zookeeper ./zookeeper \
  --namespace zookeeper --create-namespace
