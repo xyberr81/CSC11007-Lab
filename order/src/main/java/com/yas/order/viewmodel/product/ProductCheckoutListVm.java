fdc6162cd49e5dd9db42937b5ddb1ced45eb98da
xyberr <sikea0801@gmail.com>
2026-07-08 15:40:37 +0700

    Fix checkout product DTO deserialization

--- order/src/main/java/com/yas/order/viewmodel/product/ProductCheckoutListVm.java
+++ order/src/main/java/com/yas/order/viewmodel/product/ProductCheckoutListVm.java
@@ -2,9 +2,13 @@ package com.yas.order.viewmodel.product;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Builder(toBuilder = true)
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductCheckoutListVm {
     Long id;
     String name;
